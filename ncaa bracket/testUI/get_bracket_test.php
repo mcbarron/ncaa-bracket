67de4dc195cfa69c3e3368282174c421fadc5ebc
nick.peeples <nick.peeples@c1cf7beb-e129-0410-9502-5d9849a62712>
2007-02-20 06:21:47 +0000

    initial import

---
+++ ncaa bracket/testUI/get_bracket_test.php
@@ -0,0 +1,6 @@
+<?php
+require ('../includes/dbfunctions.php');
+
+echo getBracket("2007");
+echo getPosition("2007", 1);
+?>
---
+++ ncaa bracket/testUI/test_promote_pick.php
@@ -0,0 +1,6 @@
+<?php
+require ("../includes/dbfunctions.php");
+require ("../includes/class_def.php");
+
+echo promotePick(2, "tim@foobar.com", 1);
+?>
